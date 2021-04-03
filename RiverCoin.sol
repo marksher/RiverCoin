45664b5799afd8bd890c455593bae55bd625b20b
marksher <markshervey@gmail.com>
2021-04-03 19:54:38 -0400

    Create RiverCoin.sol

---
+++ RiverCoin.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
+
+
+// 1 0x197a3b2D22896C1263E97237F2B2A090847a8357
+// 2 0x48a5a9C334ed3933305D29C2E1606d47a5789920
+
+// contract address 0x7F2d97D55A42564d7D491cB5fA89d62DAFEA9A74
