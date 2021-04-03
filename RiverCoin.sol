0f166ba1a7a2f490ed458263f89440aa70d03995
marksher <markshervey@gmail.com>
2021-04-03 19:58:33 -0400

    Update RiverCoin.sol

--- RiverCoin.sol
+++ RiverCoin.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.6.2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
+// https://my-json-server.typicode.com/marksher/RiverCoin/tokens
+
+// https://testnets.opensea.io/assets/0x7F2d97D55A42564d7D491cB5fA89d62DAFEA9A74/1
 
 // 1 0x197a3b2D22896C1263E97237F2B2A090847a8357
 // 2 0x48a5a9C334ed3933305D29C2E1606d47a5789920
