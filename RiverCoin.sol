05863ebf0f65edddaec09b99b6eda511f825aec9
marksher <markshervey@gmail.com>
2021-04-03 20:02:25 -0400

    Update RiverCoin.sol

--- RiverCoin.sol
+++ RiverCoin.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.6.2;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
 
-// https://my-json-server.typicode.com/marksher/RiverCoin/tokens
-
-// https://testnets.opensea.io/assets/0x7F2d97D55A42564d7D491cB5fA89d62DAFEA9A74/1
-
+// ACCOUNTS
 // 1 0x197a3b2D22896C1263E97237F2B2A090847a8357
 // 2 0x48a5a9C334ed3933305D29C2E1606d47a5789920
+
+// https://forum.openzeppelin.com/t/create-an-nft-and-deploy-to-a-public-testnet-using-remix/6358
+
+// https://my-json-server.typicode.com/marksher/RiverCoin/tokens
+
+// Deploy: ERC721PresetMinterPauserAutoId("RiverCoin","RCN","https://my-json-server.typicode.com/marksher/RiverCoin/tokens/")
+
+// https://testnets.opensea.io/assets/0x7F2d97D55A42564d7D491cB5fA89d62DAFEA9A74/1
 
 // contract address 0x7F2d97D55A42564d7D491cB5fA89d62DAFEA9A74
